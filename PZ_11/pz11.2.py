0f02e3b83e7cc52cc5b40ab0b0d2b0705e186fd3
alisayaroshenkogo-dotcom <alisa.yaroshenko.go@gmail.com>
2026-04-04 17:52:56 +0300

    Update pz11.2.py

--- PZ_11/pz11.2.py
+++ PZ_11/pz11.2.py
@@ -1,1 +1,10 @@
+#Составить генератор (yield), который преобразует все буквенные символы в заглавные
 
+def to_upper(text):
+    for ch in text:
+        yield ch.upper()
+
+text = input("Введите текст для изменения: ")
+
+for i in to_upper(text):
+    print(i, end="")
